57964864f603e1f48a5e08ba293f65653d36e71e
Pnina Chen <pnina44440@gmail.com>
2019-05-22 00:41:13 +0300

    Initial commit

---
+++ RestaurantNew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantNew.Startup))]
+namespace RestaurantNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
